4c604d5be57637a0b62565500f3957078479d6f6
luizzeroxis <eldorado.luiz@gmail.com>
2026-08-22 14:11:32 -0300

    Add character column to font editor

---
+++ UndertaleModToolAvalonia/Helpers/CodePointToCharConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace UndertaleModToolAvalonia;
+
+public class CodePointToCharConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is ushort valueUshort)
+        {
+            return (char)valueUshort;
+        }
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string valueString)
+        {
+            if (!string.IsNullOrEmpty(valueString))
+            {
+                return (ushort)valueString[0];
+            }
+        }
+        return null;
+    }
+}
